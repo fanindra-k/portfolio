174c845a9e58c25bcf787952866e65ac11e02742
Fanindra Kumar <132550365+fanindra-k@users.noreply.github.com>
2026-07-26 20:49:05 +0530

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-module.exports = {
-  // other configurations...
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.ACCESS_KEY": JSON.stringify(process.env.ACCESS_KEY),
-    }),
-  ],
-};
